76fe364c12e25c875875ed951e44443b113c565d
Espen Hellerud <espen.hellerud@bekk.no>
2018-09-22 14:33:57 +0200

    Set mode in script

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    mode: 'development',
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
