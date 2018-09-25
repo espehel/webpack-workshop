80bc06f314e336a7aeccc579d69e131747fbd1f4
Espen Hellerud <espen.hellerud@bekk.no>
2018-09-25 14:00:22 +0200

    remove unsused import

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
